2e3d1d539a46abc7467b0c9afca372c4466800d0
Josh Surber <josh@surber.us>
2023-01-13 12:47:26 -0600

    awesome: Make non-focus windows transparant

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -569,3 +569,11 @@ client.connect_signal("focus", function(c) c.border_color = beautiful.border_foc
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
 awful.spawn.with_shell("~/.config/awesome/autorun.sh")
+client.connect_signal("focus", function(c)
+                              c.border_color = beautiful.border_focus
+                              c.opacity = 1
+                           end)
+client.connect_signal("unfocus", function(c)
+                                c.border_color = beautiful.border_normal
+                                c.opacity = 0.7
+                             end)
