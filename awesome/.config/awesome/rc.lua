fd8359f4d6db44f657e6a3069d17cea6ccbcb359
Josh Surber <josh@surber.us>
2023-01-16 02:58:30 -0600

    aweseome: Themes

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -50,7 +50,11 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
+-- beautiful.init(gears.filesystem.get_themes_dir() .. "gtk/theme.lua")
+local chosen_theme = 'gtk'
+local theme_path = string.format("%s/.config/awesome/themes/%s/theme.lua", os.getenv("HOME"), chosen_theme)
+beautiful.init(theme_path)
+
 
 -- This is used later as the default terminal and editor to run.
 terminal = "kitty"
