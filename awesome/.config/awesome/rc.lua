8f27e96d0ef72655631109c7edfebaacf4ee20d6
Josh Surber <josh@surber.us>
2023-02-01 12:19:06 -0600

    awesome: Use picom a bit more

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -566,8 +566,8 @@ client.connect_signal("mouse::enter", function(c)
     c:emit_signal("request::activate", "mouse_enter", { raise = false })
 end)
 
-client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
-client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
+-- client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
+-- client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
 
 awful.spawn.with_shell("~/.config/awesome/autorun.sh")
