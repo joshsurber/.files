67508b5ccab697ce8f242f3563733c850b3ac33f
Josh Surber <josh@surber.us>
2023-11-26 12:19:40 -0600

    awesome: remove some autorun items

--- awesome/.config/awesome/autorun.sh
+++ awesome/.config/awesome/autorun.sh
@@ -15,12 +15,12 @@ function run {
 
 run picom
 run nm-applet
-run blueman-applet
+# run blueman-applet
 # run cbatticon
 run nitrogen --restore
 # run pcmanfm --desktop
 # run pcmanfm -d
 # run volumeicon
 run xscreensaver --no-splash
-run setxkbmap -option caps:escape
+# run setxkbmap -option caps:escape
 # setxkbmap us -variant colemak
