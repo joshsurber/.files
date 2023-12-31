721aa4ff105b4a94dddc312f932ad51629f26c10
Josh Surber <josh@surber.us>
2023-12-31 10:53:11 -0600

    awesome: add binding for obsidian

--- awesome/.config/awesome/keys.lua
+++ awesome/.config/awesome/keys.lua
@@ -58,6 +58,7 @@ globalkeys = gears.table.join(
 
         -- Launcher
         awful.key(KeyOS, "a", function() awful.spawn('thunderbird') end, { description = "launch thunderbird", group = "launcher" }),
+        awful.key(CtlOS, "a", function() awful.spawn('obsidian') end, { description = "launch obsidian", group = "launcher" }),
         awful.key(KeyOS, "r", function() awful.spawn('pcmanfm') end, { description = "launch pcmanfm", group = "launcher" }),
         awful.key(CtlOS, "r", function() awful.spawn(terminal .. ' -e vifm') end, { description = "launch vifm", group = "launcher" }),
         awful.key(KeyOS, "s", function() awful.spawn('google-chrome-stable') end, { description = "launch Chrome", group = "launcher" }),
