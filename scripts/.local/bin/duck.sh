99ceb23e56708c4f61c6a23b8d6dc899cd60ba2c
Josh Surber <josh@surber.us>
2024-12-20 08:36:46 -0600

    scripts: save ducks log in .local

--- scripts/.local/bin/duck.sh
+++ scripts/.local/bin/duck.sh
@@ -1,1 +1,2 @@
-echo url="https://www.duckdns.org/update?domains=joshsurber&token=b5aef6bb-afa1-42ef-b969-b4cbe91d36e1&ip=" | curl -k -o ~/Scripts/duck.log -K -
+mkdir -p ~/.local/log
+echo url="https://www.duckdns.org/update?domains=joshsurber&token=b5aef6bb-afa1-42ef-b969-b4cbe91d36e1&ip=" | curl -k -o ~/.local/log/duck.log -K -
