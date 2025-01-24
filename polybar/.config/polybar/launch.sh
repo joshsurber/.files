e1c20e2f4e47a597f385a3697f41ad66154d916c
Josh Surber <josh@surber.us>
2025-01-23 19:30:55 -0600

    polybar: only one bar for now

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -3,7 +3,9 @@
 # Terminate already running bar instances
 # killall -q polybar
 # If all your bars have ipc enabled, you can also use
-polybar-msg cmd quit
+polybar-msg cmd quit 2>/dev/null
+
+polybar -r only 2>&1 | tee -a /tmp/polybar.log & disown
 
 if [[ $(xrandr -q |grep 'eDP-1 connected') ]]; then
     polybar -r laptop 2>&1 | tee -a /tmp/polybar.log & disown
@@ -13,5 +15,8 @@ if [[ $(xrandr -q |grep 'HDMI-1 connected') ]]; then
     polybar -r desktop 2>&1 | tee -a /tmp/polybar.log & disown
 fi
 
+if [[ $(xrandr -q |grep 'DP-1 connected') ]]; then
+    polybar -r desktop 2>&1 | tee -a /tmp/polybar.log & disown
+fi
 
 echo "Polybar launched..."
