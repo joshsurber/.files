5894e96a9a5b4dd031988ad0323e507a3eb60718
Josh Surber <josh@surber.us>
2025-01-25 13:23:04 -0600

    bspwm: add alt-space and alt-enter
    Makes it easy to launch terminal or launcher

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -7,16 +7,16 @@ polybar-msg cmd quit 2>/dev/null
 
 polybar -r only 2>&1 | tee -a /tmp/polybar.log & disown
 
-if [[ $(xrandr -q |grep 'eDP-1 connected') ]]; then
-    polybar -r laptop 2>&1 | tee -a /tmp/polybar.log & disown
-fi
+# if [[ $(xrandr -q |grep 'eDP-1 connected') ]]; then
+#     polybar -r laptop 2>&1 | tee -a /tmp/polybar.log & disown
+# fi
 
-if [[ $(xrandr -q |grep 'HDMI-1 connected') ]]; then
-    polybar -r desktop 2>&1 | tee -a /tmp/polybar.log & disown
-fi
+# if [[ $(xrandr -q |grep 'HDMI-1 connected') ]]; then
+#     polybar -r desktop 2>&1 | tee -a /tmp/polybar.log & disown
+# fi
 
-if [[ $(xrandr -q |grep 'DP-1 connected') ]]; then
-    polybar -r desktop 2>&1 | tee -a /tmp/polybar.log & disown
-fi
+# if [[ $(xrandr -q |grep 'DP-1 connected') ]]; then
+#     polybar -r desktop 2>&1 | tee -a /tmp/polybar.log & disown
+# fi
 
 echo "Polybar launched..."
