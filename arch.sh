153641ed2e1408594b209fe5ad2288d681756c37
Josh Surber <josh@surber.us>
2025-07-23 00:01:31 -0500

    Remove superfluous bindings
    If i wanna launch something other than browser, terminal, or file
    explorer, I can just use rofi

--- arch.sh
+++ arch.sh
@@ -1,16 +1,16 @@
-sudo pacman -S --needed - <./pkgs.arch
+sudo pacman -S --needed - < ./pkgs.arch
 git clone git@github.com:joshsurber/nvim.git ~/.config/nvim
 bat cache --build
 
 mkdir -p ~/Desktop ~/Documents ~/Downloads ~/Google ~/Music ~/Pictures ~/Projects ~/Public ~/Templates ~/Videos ~/.config/rclone
-cat rclone.txt >~/.config/rclone/rclone.conf
+cat rclone.txt > ~/.config/rclone/rclone.conf
 rclone config reconnect drive:
 
 make
 
-echo 'source ~/.bash/source' >>~/.bashrc
-echo 'source ~/.bash/profile' >>~/.profile
-echo 'source ~/.bash/logout' >>~/.bash_logout
+echo 'source ~/.bash/source' >> ~/.bashrc
+echo 'source ~/.bash/profile' >> ~/.profile
+echo 'source ~/.bash/logout' >> ~/.bash_logout
 
 sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 yay -Y --gendb
