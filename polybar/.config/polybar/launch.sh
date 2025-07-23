f33d040caf5055df2b94e39c6306af6fd5b45210
Josh Surber <josh@surber.us>
2025-07-22 22:23:28 -0500

    Local polybar tweaks

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -3,20 +3,21 @@
 # Terminate already running bar instances
 # killall -q polybar
 # If all your bars have ipc enabled, you can also use
-polybar-msg cmd quit 2>/dev/null
+polybar-msg cmd quit 2> /dev/null
 
 # polybar -r only 2>&1 | tee -a /tmp/polybar.log & disown
 
-if [[ $(xrandr -q |grep 'eDP-1 connected') ]]; then
-    polybar -r laptop 2>&1 | tee -a /tmp/polybar.log & disown
+if [[ $(xrandr -q | grep 'eDP-1 connected') ]]; then
+  polybar -r laptop 2>&1 | tee -a /tmp/polybar.log &
+  disown
+elif [[ $(xrandr -q | grep 'DP-1 connected') ]]; then
+  polybar -r only 2>&1 | tee -a /tmp/polybar.log &
+  disown
 fi
 
-if [[ $(xrandr -q |grep 'HDMI-1 connected') ]]; then
-    polybar -r desktop 2>&1 | tee -a /tmp/polybar.log & disown
-fi
-
-if [[ $(xrandr -q |grep 'DP-1 connected') ]]; then
-    polybar -r only 2>&1 | tee -a /tmp/polybar.log & disown
+if [[ $(xrandr -q | grep 'HDMI-1 connected') ]]; then
+  polybar -r desktop 2>&1 | tee -a /tmp/polybar.log &
+  disown
 fi
 
 # if [[ $(xrandr -q |grep 'DP-1 connected') ]]; then
