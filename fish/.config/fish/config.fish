996542de22b4aa6db25750fb8325a2d6a4ea8a54
Josh Surber <josh@surber.us>
2025-07-29 04:11:11 -0500

    Get zoxide back, replacing cd command

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,5 +2,7 @@
 # set BROWSER chromium
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    zoxide init fish --cmd cd | source
+    # zoxide init fish | source
     starship init fish | source
 end
