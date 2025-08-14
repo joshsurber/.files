ee8145c4cd5e80b5d4f8dd3ac4785f63b9cc3151
Josh Surber <josh@surber.us>
2025-08-14 16:47:54 -0500

    Rename keyd to keyedit

--- fish/.config/fish/conf.d/abbrev.fish
+++ fish/.config/fish/conf.d/abbrev.fish
@@ -12,7 +12,7 @@ abbr lla 'ls -la'
 abbr serve live-server
 abbr lamp sudo /opt/lampp/lampp
 abbr drive 'rclone mount drive: ~/Google --daemon'
-abbr keyd 'sudoedit /etc/keyd/default.conf && sudo keyd reload'
+abbr keyedit 'sudoedit /etc/keyd/default.conf && sudo keyd reload'
 abbr ducks 'du -cks *|sort -rn|head -11'
 function multicd
     echo cd (string repeat -n (math (string length -- $argv[1]) - 1) ../)
