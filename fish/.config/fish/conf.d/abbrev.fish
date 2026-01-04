9f143e47ccd409126d04919a856a970e6373dfa9
Josh Surber <josh@surber.us>
2026-01-03 21:10:55 -0600

    Initial divergance from main for chromeos

--- fish/.config/fish/conf.d/abbrev.fish
+++ fish/.config/fish/conf.d/abbrev.fish
@@ -5,13 +5,14 @@ abbr grep rg
 abbr ls eza
 abbr o xdg-open
 abbr v nvim
-abbr i sudo pacman -S
+abbr i sudo apt install
+abbr b brew install
 abbr ll 'ls -l'
 abbr la 'ls -a'
 abbr lla 'ls -la'
 abbr serve live-server
 abbr lamp sudo /opt/lampp/lampp
-abbr drive 'rclone mount drive: ~/Google --daemon'
+# abbr drive 'rclone mount drive: ~/Google --daemon'
 abbr keyedit 'sudoedit /etc/keyd/default.conf && sudo keyd reload'
 abbr ducks 'du -cks *|sort -rn|head -11'
 function multicd
