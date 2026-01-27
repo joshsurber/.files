653575b6501c49c22019e127a34db1794877b609
Josh Surber <josh@surber.us>
2026-01-27 14:13:36 -0600

    Map "tn" to escape in fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,12 @@
 # set EDITOR nvim
 # set BROWSER chromium
 if status is-interactive
+
+    function fish_user_key_bindings
+        # Maps 'tn' in insert mode to switch to normal mode
+        bind -M insert tn 'set fish_bind_mode default; commandline -f backward-char repaint-mode'
+    end
+
     # Commands to run in interactive sessions can go here
     zoxide init fish --cmd cd | source
     # zoxide init fish | source
